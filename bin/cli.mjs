b7d26099841335bf60c8c5e3d4d5750dc9f52d08
Jeff Haynie <jhaynie@pinpoint.com>
2021-08-04 20:44:52 -0500

    Refactor to work with @pinpt/cli so we can share code between both projects

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -1,23 +1,43 @@
 #!/usr/bin/env node
 
-import { showWelcomeMessage } from '../lib/show-welcome-message.mjs';
+import path from 'path';
+import { showWelcomeMessage } from '@pinpt/cli/lib/show-welcome-message.mjs';
 import { showSuccessMessage } from '../lib/show-success-message.mjs';
 import { installProjectDependencies } from '../lib/install-project-dependencies.mjs';
 import { copyAndFilterFiles } from '../lib/copy-template-files.mjs';
-import { showHelpMessage } from '../lib/show-help-message.mjs';
-import { showVersionMessage } from '../lib/show-version-message.mjs';
-import { getSignup } from '../lib/signup.mjs';
-import { error } from '../lib/util.mjs';
+import { showHelpMessage } from '@pinpt/cli/lib/show-help-message.mjs';
+import { showVersionMessage } from '@pinpt/cli/lib/show-version-message.mjs';
+import { init, error } from '@pinpt/cli/lib/util.mjs';
+import { getArgs } from '@pinpt/cli/lib/args.mjs';
+import { showSignup } from '../lib/signup.mjs';
+
+export const optionSpec = {
+	slug: { type: String, description: 'The slug for your Pinpoint site' },
+	help: { type: Boolean, description: 'This menu :/', alias: 'h', default: false },
+	version: { type: Boolean, description: 'Print the version and exit', alias: 'v', default: false },
+	host: { type: String, description: 'The api host', hidden: true, default: 'api.pinpoint.com' },
+	'dry-run': { type: Boolean, description: `Run the command but don't actually do anything`, default: false },
+	'use-npm': { type: Boolean, description: 'Use npm even if running from yarn', default: false },
+	dir: {
+		type: String,
+		description: 'The output folder to generate your project into',
+		default: path.resolve(process.cwd()),
+	},
+	debug: { type: Boolean, description: 'Turn on verbose debug logging', default: false },
+};
 
 (async () => {
 	try {
-		await showHelpMessage();
-		await showVersionMessage();
+		const [options, args] = getArgs(optionSpec);
+		const cmd = { args, options, optionSpec, meta: import.meta };
+		init(cmd);
+		await showVersionMessage(cmd);
+		await showHelpMessage(cmd);
 		await showWelcomeMessage();
-		await getSignup();
-		await copyAndFilterFiles();
-		await installProjectDependencies();
-		await showSuccessMessage();
+		await showSignup(cmd);
+		await copyAndFilterFiles(cmd);
+		await installProjectDependencies(cmd);
+		await showSuccessMessage(cmd);
 	} catch (ex) {
 		error(ex.message, true);
 	}
